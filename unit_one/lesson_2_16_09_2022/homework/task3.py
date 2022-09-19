f1fa22ecd854f41723e50daf9cda708e47aa6b1a
Bogatenkov_alex <super.baaars@yandex.ru>
2022-09-19 18:58:43 +0300

    Исправленное 3 задание

--- unit_one/lesson_2_16_09_2022/homework/task3.py
+++ unit_one/lesson_2_16_09_2022/homework/task3.py
@@ -4,5 +4,10 @@ age = 36.6
 temperature = 25
 age = 25
 temperature = 36.6
-print("Возраст", age)
-print("Температура", temperature)
+#print("Возраст", age)
+#print("Температура", temperature)
+a = age
+age = temperature
+temperature = a
+print(age)
+print(temperature)
