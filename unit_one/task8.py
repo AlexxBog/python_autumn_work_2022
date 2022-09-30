d109704dd90bce73fcbb40d7a71fa20129a96179
Bogatenkov_alex <super.baaars@yandex.ru>
2022-09-30 10:05:19 +0300

    1

--- unit_one/task8.py
+++ unit_one/task8.py
@@ -1,21 +1,37 @@
 #todo: Даны переменные A, B, C. Написать код который меняет местами переменные таким образом
 # чтобы значения в переменных были расположены по возрастанию
 # Пример 1:
-A = 10
-B = 3
-C = 7
+a = 10
+b = 3
+c = 7
 # Итоговый результат должен быть:
 # A = 3
 # B = 7
 # C = 10
 
 # Пример 2:
-A = 2
-B = 10
-C = 7
+#A = 2
+#B = 10
+#C = 7
 # Итоговый результат должен быть:
 # A = 2
 # B = 7
 # C = 10
 
-variable_1 =
+a = int(input('Введите А '))
+b = int(input('Введите Б '))
+c = int(input('Введите С '))
+if a >= b and a >= c and b >= c:
+    print(c, a, b)
+elif a >= b and a >= c and b <= c:
+    print(b, c, a)
+elif b >= a and b >= c and a >= c:
+    print(c, a, b)
+elif b >= a and b >= c and a <= c:
+    print(a, c, b)
+elif c >= a and c >= b and a >= b:
+    print(b, a, c)
+elif c >= a and c >= b and a <= b:
+    print(a, b, c)
+
+
