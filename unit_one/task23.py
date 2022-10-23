02405b97699ac78ed4d5084b059af48da3c7ff12
Bogatenkov_alex <super.baaars@yandex.ru>
2022-10-23 21:17:41 +0300

    last1

--- unit_one/task23.py
+++ unit_one/task23.py
@@ -1,6 +1,24 @@
 #todo: Взлом шифра
-Вы знаете, что фраза зашифрована кодом цезаря с неизвестным сдвигом.
-Попробуйте все возможные сдвиги и расшифруйте фразу.
+#Вы знаете, что фраза зашифрована кодом цезаря с неизвестным сдвигом.
+#Попробуйте все возможные сдвиги и расшифруйте фразу.
 
 
-grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin.
+#grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin.
+
+line = "grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin."
+
+
+alphabet_EN = []
+for i in range(97, 123):
+    alphabet_EN.append(chr(i))
+
+
+for i in range(1, len(alphabet_EN)):
+    res = []
+    for symbol in line:
+        if symbol in alphabet_EN:
+            shift = alphabet_EN.index(symbol) - i
+            res.append(alphabet_EN[shift])
+        else:
+            res.append(symbol)
+    print("".join(res))
--- unit_one/task25.py
+++ unit_one/task25.py
@@ -1,8 +1,25 @@
 #todo: Убрать повторяющиеся буквы и лишние символы
-Построить по ключевой фразе часть алфавита. Взять все буквы по одному разу. Не буквы убрать.
-Буквы должны идти в том порядке, в котором встретились во фразе в первый раз.
+#Построить по ключевой фразе часть алфавита. Взять все буквы по одному разу. Не буквы убрать.
+#Буквы должны идти в том порядке, в котором встретились во фразе в первый раз.
+#
+#Input             	            Output
+#
+#apple	                        aple
+#25.04.2022 Good morning !!	    godmrni
 
-Input             	            Output
+alphabet_EN = []
 
-apple	                        aple
-25.04.2022 Good morning !!	    godmrni
+for i in range(97, 123):
+    alphabet_EN.append(chr(i))
+print(alphabet_EN)
+
+test_1 = "apple"
+test_2 = "25.04.2022 Good morning !!"
+
+res = []
+for symbol in test_2.lower():
+    if symbol in alphabet_EN:
+        if symbol not in res:
+            res.append(symbol)
+
+print("".join(res))
